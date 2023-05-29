d79c3b8dfdebebf7a30d585cdf01f7350993dd2c
Giulia9999 <120981976+Giulia9999@users.noreply.github.com>
2023-05-29 13:05:13 +0200

    Update BasicController.java

--- deploy01/src/main/java/com/example/deploy01/BasicController.java
+++ deploy01/src/main/java/com/example/deploy01/BasicController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/")
 public class BasicController {
-   @Value("${com.example.deploy01.devName}")
+   @Value("${devName}")
     private String devName;
 
     @GetMapping
